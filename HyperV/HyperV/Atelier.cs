d06dd9aeeb040c88bf3247285618a5e90cd560f3
Anthony <Anthony@BUR-5>
2017-02-06 10:55:49 -0500

    Niveau load un modele 3D mais ya pas de texture dessus

--- HyperV/HyperV/Atelier.cs
+++ HyperV/HyperV/Atelier.cs
@@ -36,12 +36,14 @@ namespace HyperV
             Components.Add(new AfficheurFPS(this, "Arial", Color.Red, INTERVALLE_CALCUL_FPS));
             Components.Add(new Jeu(this));
             Components.Add(new Player(this, INTERVALLE_MAJ_STANDARD));
+            Components.Add(new Niveau(this, "Models_Rockwall", "Texture_Rockwall", Vector3.Zero));
 
             Services.AddService(typeof(Random), new Random());
             Services.AddService(typeof(RessourcesManager<SpriteFont>), new RessourcesManager<SpriteFont>(this, "Fonts"));
             Services.AddService(typeof(RessourcesManager<SoundEffect>), new RessourcesManager<SoundEffect>(this, "Sounds"));
             Services.AddService(typeof(RessourcesManager<Song>), new RessourcesManager<Song>(this, "Songs"));
             Services.AddService(typeof(RessourcesManager<Texture2D>), new RessourcesManager<Texture2D>(this, "Textures"));
+            Services.AddService(typeof(RessourcesManager<Model>), new RessourcesManager<Model>(this, "Models"));
             Services.AddService(typeof(InputManager), GestionInput);
             Services.AddService(typeof(CaméraSubjective), CaméraJeu);
             Services.AddService(typeof(SpriteBatch), new SpriteBatch(GraphicsDevice));
