9ff8bea76ce3ce0687beaf9fc4f6ba4248b95830
Anthony <Anthony@BUR-5>
2017-02-20 10:01:56 -0500

    k

--- HyperV/HyperV/Skybox.cs
+++ HyperV/HyperV/Skybox.cs
@@ -1,12 +1,6 @@
 ﻿using AtelierXNA;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Content;
-using AtelierXNA;
 
 namespace HyperV
 {
