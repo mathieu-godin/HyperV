5a1e87b5f3b027a3c6cb66fd66a0a3cc0c6fbbd4
Mathieu Godin <matgodin@videotron.ca>
2017-03-28 01:52:44 -0400

    First water push

--- HyperV/HyperV/Atelier.cs
+++ HyperV/HyperV/Atelier.cs
@@ -56,10 +56,10 @@ namespace HyperV
             PériphériqueGraphique.SynchronizeWithVerticalRetrace = false;
             IsFixedTimeStep = false;
             IsMouseVisible = false;
-            //PériphériqueGraphique.PreferredBackBufferHeight = 800;
-            //PériphériqueGraphique.PreferredBackBufferWidth = 1500;
-            PériphériqueGraphique.PreferredBackBufferHeight = 500;
-            PériphériqueGraphique.PreferredBackBufferWidth = 1000;
+            PériphériqueGraphique.PreferredBackBufferHeight = 800;
+            PériphériqueGraphique.PreferredBackBufferWidth = 1500;
+            //PériphériqueGraphique.PreferredBackBufferHeight = 500;
+            //PériphériqueGraphique.PreferredBackBufferWidth = 1000;
         }
 
         Gazon Gazon { get; set; }
@@ -88,8 +88,8 @@ namespace HyperV
 
         void LoadSettings()
         {
-            //StreamReader reader = new StreamReader("F:/programmation clg/quatrième session/WPFINTERFACE/Launching Interface/Saves/Settings.txt");
-            StreamReader reader = new StreamReader("C:/Users/Mathieu/Source/Repos/WPFINTERFACE/Launching Interface/Saves/Settings.txt");
+            StreamReader reader = new StreamReader("F:/programmation clg/quatrième session/WPFINTERFACE/Launching Interface/Saves/Settings.txt");
+            //StreamReader reader = new StreamReader("C:/Users/Mathieu/Source/Repos/WPFINTERFACE/Launching Interface/Saves/Settings.txt");
             string line = reader.ReadLine();
             string[] parts = line.Split(new string[] { ": " }, StringSplitOptions.None);
             MediaPlayer.Volume = int.Parse(parts[1]) / 100.0f;
@@ -125,12 +125,12 @@ namespace HyperV
 
         void LoadSave()
         {
-            //StreamReader reader = new StreamReader("F:/programmation clg/quatrième session/WPFINTERFACE/Launching Interface/Saves/save.txt");
-            StreamReader reader = new StreamReader("C:/Users/Mathieu/Source/Repos/WPFINTERFACE/Launching Interface/Saves/save.txt");
+            StreamReader reader = new StreamReader("F:/programmation clg/quatrième session/WPFINTERFACE/Launching Interface/Saves/save.txt");
+            //StreamReader reader = new StreamReader("C:/Users/Mathieu/Source/Repos/WPFINTERFACE/Launching Interface/Saves/save.txt");
             SaveNumber = int.Parse(reader.ReadLine());
             reader.Close();
-            //reader = new StreamReader("F:/programmation clg/quatrième session/WPFINTERFACE/Launching Interface/Saves/save" + SaveNumber.ToString() + ".txt");
-            reader = new StreamReader("C:/Users/Mathieu/Source/Repos/WPFINTERFACE/Launching Interface/Saves/save" + SaveNumber.ToString() + ".txt");
+            reader = new StreamReader("F:/programmation clg/quatrième session/WPFINTERFACE/Launching Interface/Saves/save" + SaveNumber.ToString() + ".txt");
+            //reader = new StreamReader("C:/Users/Mathieu/Source/Repos/WPFINTERFACE/Launching Interface/Saves/save" + SaveNumber.ToString() + ".txt");
             string line = reader.ReadLine();
             string[] parts = line.Split(new char[] { ' ' });
             Level = int.Parse(parts[1]);
@@ -184,8 +184,8 @@ namespace HyperV
 
         void Save()
         {
-            //StreamWriter writer = new StreamWriter("F:/programmation clg/quatrième session/WPFINTERFACE/Launching Interface/Saves/pendingsave.txt");
-            StreamWriter writer = new StreamWriter("C:/Users/Mathieu/Source/Repos/WPFINTERFACE/Launching Interface/Saves/pendingsave.txt");
+            StreamWriter writer = new StreamWriter("F:/programmation clg/quatrième session/WPFINTERFACE/Launching Interface/Saves/pendingsave.txt");
+            //StreamWriter writer = new StreamWriter("C:/Users/Mathieu/Source/Repos/WPFINTERFACE/Launching Interface/Saves/pendingsave.txt");
 
             writer.WriteLine("Level: " + Level.ToString());
             if (Camera != null)
@@ -207,6 +207,7 @@ namespace HyperV
         HeightMap HeightMap { get; set; }
         LifeBar[] LifeBars { get; set; }
         Afficheur3D Display3D { get; set; }
+        Water Water { get; set; }
 
         void Level2(bool usePosition)
         {
@@ -235,9 +236,11 @@ namespace HyperV
             Components.Add(Mill);
             Mill.AddLabel();
             Services.AddService(typeof(Mill), Mill);
-            HeightMap = new HeightMap(this, 1, Vector3.Zero, Vector3.Zero, new Vector3(10000, 100, 10000), "HeightMap", "Ceiling");
-            //Components.Add(HeightMap);
+            HeightMap = new HeightMap(this, 1, Vector3.Zero, Vector3.Zero, new Vector3(10000, 1000, 10000), "HeightMap", "Ceiling");
+            Components.Add(HeightMap);
             Services.AddService(typeof(HeightMap), HeightMap);
+            Water = new Water(this, 1f, Vector3.Zero, new Vector3(10000, 300, 200), new Vector2(10000, 10000), FpsInterval);
+            Components.Add(Water);
             Boss.AddFireball();
             Boss.AddLabel();
             Components.Add(LifeBars[0]);
@@ -647,8 +650,8 @@ namespace HyperV
             {
                 Save();
                 TakeAScreenshot();
-                //string path = "F:/programmation clg/quatrième session/WPFINTERFACE/Launching Interface/bin/Debug/Launching Interface.exe";
-                string path = "C:/Users/Mathieu/Source/Repos/WPFINTERFACE/Launching Interface/bin/Debug/Launching Interface.exe";
+                string path = "F:/programmation clg/quatrième session/WPFINTERFACE/Launching Interface/bin/Debug/Launching Interface.exe";
+                //string path = "C:/Users/Mathieu/Source/Repos/WPFINTERFACE/Launching Interface/bin/Debug/Launching Interface.exe";
                 ProcessStartInfo p = new ProcessStartInfo();
                 p.FileName = path;
                 p.WorkingDirectory = System.IO.Path.GetDirectoryName(path);
@@ -675,8 +678,8 @@ namespace HyperV
             {
                 try
                 {
-                    //stream = File.OpenWrite("F:/programmation clg/quatrième session/WPFINTERFACE/Launching Interface/Saves/pendingscreenshot.png");
-                    stream = File.OpenWrite("C:/Users/Mathieu/Source/Repos/WPFINTERFACE/Launching Interface/Saves/pendingscreenshot.png");
+                    stream = File.OpenWrite("F:/programmation clg/quatrième session/WPFINTERFACE/Launching Interface/Saves/pendingscreenshot.png");
+                    //stream = File.OpenWrite("C:/Users/Mathieu/Source/Repos/WPFINTERFACE/Launching Interface/Saves/pendingscreenshot.png");
                 }
                 catch (IOException e)
                 {
--- HyperV/HyperV/Water.cs
+++ HyperV/HyperV/Water.cs
@@ -1,3 +1,13 @@
+/*
+Water.cs
+--------
+
+By Mathieu Godin
+
+Role : Used to create a flat water surface.
+
+Created : 3/28/17
+*/
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +18,7 @@ using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
+using AtelierXNA;
 
 
 namespace HyperV
@@ -15,34 +26,171 @@ namespace HyperV
     /// <summary>
     /// This is a game component that implements IUpdateable.
     /// </summary>
-    public class Water : Microsoft.Xna.Framework.GameComponent
+    public class Water : PrimitiveDeBase
     {
-        public Water(Game game)
-            : base(game)
+        Vector3 Position { get; set; }
+        float IntervalleMAJ { get; set; }
+        protected InputManager GestionInput { get; private set; }
+        float TempsÉcouléDepuisMAJ { get; set; }
+
+        const int NB_TRIANGLES = 2;
+        protected Vector3[,] PtsSommets { get; private set; }
+        Vector3 Origine { get; set; }
+        Vector2 Delta { get; set; }
+        protected BasicEffect EffetDeBase { get; private set; }
+
+        VertexPositionColor[] Sommets { get; set; }
+        //VertexPositionTexture[] Sommets { get; set; }
+        BlendState GestionAlpha { get; set; }
+
+        //Vector2[,] PtsTexture { get; set; }
+        Afficheur3D Display3D { get; set; }
+
+        public Vector3 GetPositionAvecHauteur(Vector3 position, int hauteur)
         {
-            // TODO: Construct any child components here
+            Vector3 positionAvecHauteur;
+            if (EstEntre(position.Z, PtsSommets[0, 0].Z, PtsSommets[PtsSommets.GetLength(0) - 1, PtsSommets.GetLength(1) - 1].Z) &&
+                EstEntre(position.X, PtsSommets[0, 0].X, PtsSommets[PtsSommets.GetLength(0) - 1, PtsSommets.GetLength(1) - 1].X))
+            {
+                positionAvecHauteur = new Vector3(position.X, PtsSommets[0, 0].Y + hauteur, position.Z);
+            }
+            else
+            {
+                positionAvecHauteur = position;
+            }
+            return positionAvecHauteur;
         }
 
-        /// <summary>
-        /// Allows the game component to perform any initialization it needs to before starting
-        /// to run.  This is where it can query for any required services and load content.
-        /// </summary>
+        private bool EstEntre(float valeur, float borneA, float borneB)
+        {
+            return (valeur >= borneA && valeur <= borneB || valeur <= borneA && valeur >= borneB);
+        }
+
+        public Water(Game jeu, float homothétieInitiale, Vector3 rotationInitiale,
+                     Vector3 positionInitiale, Vector2 étendue,
+                     float intervalleMAJ)
+            : base(jeu, homothétieInitiale, rotationInitiale, positionInitiale)
+        {
+            IntervalleMAJ = intervalleMAJ;
+            Delta = new Vector2(étendue.X, étendue.Y);
+            Origine = new Vector3(-Delta.X / 2, 0, -Delta.Y / 2); //pour centrer la primitive au point (0,0,0)
+        }
+
         public override void Initialize()
         {
-            // TODO: Add your initialization code here
-
+            NbSommets = NB_TRIANGLES + 2;
+            PtsSommets = new Vector3[2, 2];
+            CréerTableauPoints();
+            CréerTableauSommets();
+            Position = PositionInitiale;
+            Display3D = Game.Services.GetService(typeof(Afficheur3D)) as Afficheur3D;
             base.Initialize();
         }
 
-        /// <summary>
-        /// Allows the game component to update itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
-        public override void Update(GameTime gameTime)
+        private void CréerTableauPoints()
         {
-            // TODO: Add your update code here
+            PtsSommets[0, 0] = new Vector3(Origine.X, Origine.Y, Origine.Z);
+            PtsSommets[1, 0] = new Vector3(Origine.X - Delta.X, Origine.Y, Origine.Z);
+            PtsSommets[0, 1] = new Vector3(Origine.X, Origine.Y, Origine.Z + Delta.Y);
+            PtsSommets[1, 1] = new Vector3(Origine.X - Delta.X, Origine.Y, Origine.Z + Delta.Y);
+        }
 
-            base.Update(gameTime);
+        protected void CréerTableauSommets()
+        {
+            //PtsTexture = new Vector2[2, 2];
+            Sommets = new VertexPositionColor[NbSommets];
+            //Sommets = new VertexPositionTexture[NbSommets];
+            //CréerTableauPointsTexture();
+        }
+
+        //private void CréerTableauPointsTexture()
+        //{
+        //    PtsTexture[0, 0] = new Vector2(0, 1);
+        //    PtsTexture[1, 0] = new Vector2(1, 1);
+        //    PtsTexture[0, 1] = new Vector2(0, 0);
+        //    PtsTexture[1, 1] = new Vector2(1, 0);
+        //}
+
+        protected override void LoadContent()
+        {
+            EffetDeBase = new BasicEffect(GraphicsDevice);
+            InitialiserParamètresEffetDeBase();
+            base.LoadContent();
+        }
+
+        protected void InitialiserParamètresEffetDeBase()
+        {
+            EffetDeBase.VertexColorEnabled = true;
+            //EffetDeBase.TextureEnabled = true;
+            //EffetDeBase.Texture = TextureTuile;
+            //GestionAlpha = BlendState.AlphaBlend; // Attention à ceci...
+        }
+
+        protected override void InitialiserSommets() // Est appelée par base.Initialize()
+        {
+            int NoSommet = -1;
+            for (int j = 0; j < 1; ++j)
+            {
+                for (int i = 0; i < 2; ++i)
+                {
+                    Sommets[++NoSommet] = new VertexPositionColor(PtsSommets[i, j], Color.Blue);
+                    Sommets[++NoSommet] = new VertexPositionColor(PtsSommets[i, j + 1], Color.Blue);
+                    //Sommets[++NoSommet] = new VertexPositionTexture(PtsSommets[i, j], PtsTexture[i, j]);
+                    //Sommets[++NoSommet] = new VertexPositionTexture(PtsSommets[i, j + 1], PtsTexture[i, j + 1]);
+                }
+            }
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            //BlendState oldBlendState = GraphicsDevice.BlendState; // ... et à cela!
+            //GraphicsDevice.BlendState = GestionAlpha;
+            //EffetDeBase.World = GetMonde();
+            //EffetDeBase.View = CaméraJeu.Vue;
+            //EffetDeBase.Projection = CaméraJeu.Projection;
+            //foreach (EffectPass passeEffet in EffetDeBase.CurrentTechnique.Passes)
+            //{
+            //    passeEffet.Apply();
+            //    DessinerTriangleStrip();
+            //}
+            //GraphicsDevice.BlendState = oldBlendState;
+            RasterizerState s;// = Display3D.JeuRasterizerState;
+            s = new RasterizerState();
+            s.CullMode = CullMode.None;
+            s.FillMode = FillMode.Solid;
+            Game.GraphicsDevice.RasterizerState = s;
+
+            EffetDeBase.World = GetMonde();
+            EffetDeBase.View = CaméraJeu.Vue;
+            EffetDeBase.Projection = CaméraJeu.Projection;
+            foreach (EffectPass passeEffet in EffetDeBase.CurrentTechnique.Passes)
+            {
+                passeEffet.Apply();
+                DessinerTriangleStrip();
+            }
+
+            Game.GraphicsDevice.RasterizerState = Display3D.JeuRasterizerState;
+        }
+
+        protected void DessinerTriangleStrip()
+        {
+            GraphicsDevice.DrawUserPrimitives<VertexPositionColor>(PrimitiveType.TriangleStrip, Sommets, 0, NB_TRIANGLES);
+            //GraphicsDevice.DrawUserPrimitives<VertexPositionTexture>(PrimitiveType.TriangleStrip, Sommets, 0, NB_TRIANGLES);
+        }
+
+        public Vector3 GetPositionWithHeight(Vector3 position, int hauteur)
+        {
+            Vector3 positionAvecHauteur;
+            if (EstEntre(position.Z, PtsSommets[0, 0].Z, PtsSommets[PtsSommets.GetLength(0) - 1, PtsSommets.GetLength(1) - 1].Z) &&
+                EstEntre(position.X, PtsSommets[0, 0].X, PtsSommets[PtsSommets.GetLength(0) - 1, PtsSommets.GetLength(1) - 1].X))
+            {
+                positionAvecHauteur = new Vector3(position.X, PtsSommets[0, 0].Y + hauteur, position.Z);
+            }
+            else
+            {
+                positionAvecHauteur = position;
+            }
+            return positionAvecHauteur;
         }
     }
 }
