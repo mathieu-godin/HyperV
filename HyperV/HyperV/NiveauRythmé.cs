456d829bfc7dcedba4bcf381a74c274e472eb7bf
Raphaël Brulé <raphael_brule@hotmail.com>
2017-04-30 19:34:09 -0400

    ok

--- HyperV/HyperV/NiveauRythmé.cs
+++ HyperV/HyperV/NiveauRythmé.cs
@@ -195,12 +195,12 @@ namespace HyperV
                 cpt = 121;
                 Game.Components.Remove(MurÀEnlever[0]);
                 ListePortails.Add(new Portal(Game, 1, new Vector3(0, 1.570796f, 0),
-                                  new Vector3(170, -60, -10), new Vector2(40, 40), "Blanc",
+                                  new Vector3(170, -60, -10), new Vector2(40, 40), "Transparent",
                                   1, IntervalleMAJ));
                 Game.Components.Add(ListePortails.Last());
             }
 
-            if (cpt > 120)
+            if (cpt > 120 || cpt == 60)
             {
                 if (!NiveauEstTerminé)
                 {
