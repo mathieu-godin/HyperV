198f7470736899a77b61fa7f8c9c06cb9103077e
Mathieu Godin <matgodin@videotron.ca>
2017-05-02 11:22:52 -0400

    same thing for the portal

--- HyperV/HyperV/Portal.cs
+++ HyperV/HyperV/Portal.cs
@@ -96,8 +96,8 @@ namespace HyperV
             }
             else if (RotationInitiale.Y == 1.570796f)
             {
-                FirstVertex = new Vector2(PositionInitiale.X, PositionInitiale.Z) + new Vector2(-PtsSommets[0, 0].X, PtsSommets[0, 0].Z);
-                SecondVertex = new Vector2(PositionInitiale.X, PositionInitiale.Z) + new Vector2(PtsSommets[1, 1].X, -PtsSommets[1, 1].Z); // TO FINISH
+                FirstVertex = new Vector2(PositionInitiale.X, PositionInitiale.Z) + new Vector2(PtsSommets[0, 0].Z, PtsSommets[0, 0].X);
+                SecondVertex = new Vector2(PositionInitiale.X, PositionInitiale.Z) + new Vector2(PtsSommets[1, 1].Z, -PtsSommets[1, 1].X);
             }
             else if (RotationInitiale.Y == 0)
             {
